594dc6e59d7f678c0d630b92eb723d09fd27b5c3
nvshibu <shibucite@gmail.com>
2023-03-21 14:03:05 +0530

    relocated all files

--- research/js/lightbox/custom.js
+++
@@ -1,1 +0,0 @@
-!function(n){"use strict";n(document).ready(function(){n(".fancybox").fancybox(),n("#fancybox-manual-b").click(function(){n.fancybox.open({href:"",type:"iframe",padding:5})})})}(jQuery);
